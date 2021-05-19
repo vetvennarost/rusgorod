ca08ec4de0132ee31e2355e15a3245d2cd92fa13
Vetven <vetvennarost@gmail.com>
2021-05-19 10:39:29 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@
 const dataLocation = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRgKZ2dZRQBHXJz52RYBn59O9YszkZgB2H-hQI0NOCVe4yOop83lOOE0xGH_ZmjH5ScUThSi9fCFLwm/pub?gid=0&single=true&output=csv';
 
 // Map's initial center, and zoom level
-const mapCenter = [41.65, -72.7];
+const mapCenter = [55, 37];
 const mapZoom = 9;
 
 // Marker icon height and width
