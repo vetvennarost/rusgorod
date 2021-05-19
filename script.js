e053bba5569e3cba4a869096e9f3c064523fda66
Vetven <vetvennarost@gmail.com>
2021-05-19 14:09:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -292,6 +292,13 @@ L.tileLayer.colorFilter(
 	}
 )
 .addTo(map);
+	
+	
+	  L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 19
+  }).addTo(map);
 
   loadData(dataLocation);
 
